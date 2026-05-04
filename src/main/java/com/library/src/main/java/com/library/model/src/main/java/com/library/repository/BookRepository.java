2f367a7fef0442bdebb341199421a7143ee943c2
mshiraza21-hub <mshiraza21@gmail.com>
2026-05-04 16:11:31 +0530

    Create BookRepository.java

---
+++ src/main/java/com/library/src/main/java/com/library/model/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package com.library.repository;
+
+import com.library.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    // Search by title or author (case insensitive)
+    List<Book> findByTitleContainingIgnoreCaseOrAuthorContainingIgnoreCase(String title, String author);
+}
